eb80414b75b365a75ce7753495338a2d735abbbc
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-18 23:52:33 +0200

    Delete cppcreator.sh

--- cppcreator.sh
+++
@@ -1,7 +0,0 @@
-cp /Users/$USER/.hidescript/templates/template.cpp $1.cpp
-cp /Users/$USER/.hidescript/templates/template.hpp $1.hpp
-cp /Users/$USER/.hidescript/templates/Makefile .
-sed -i '' "s/template/$1/g" $1.cpp
-sed -i '' "s/template/$1/g" $1.hpp
-val=$(echo "$1" | tr '[a-z]' '[A-Z]')
-sed -i '' "s/TEMPLATE/$val/g" $1.hpp
