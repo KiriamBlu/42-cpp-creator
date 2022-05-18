1563b0d7cc10f71eaa971f30c75831045fc0ce6e
Kiriam <kiriamblu@gmail.com>
2022-05-18 23:56:34 +0200

    Updated

--- cpptempcreator/cppcreator.sh
+++ cpptempcreator/cppcreator.sh
@@ -1,6 +1,6 @@
-cp /Users/$USER/.hidescript/templates/template.cpp $1.cpp
-cp /Users/$USER/.hidescript/templates/template.hpp $1.hpp
-cp /Users/$USER/.hidescript/templates/Makefile .
+cp /Users/$USER/cpptempcreator/templates/template.cpp $1.cpp
+cp /Users/$USER/cpptempcreator/templates/template.hpp $1.hpp
+cp /Users/$USER/cpptempcreator/templates/Makefile .
 sed -i '' "s/template/$1/g" $1.cpp
 sed -i '' "s/template/$1/g" $1.hpp
 val=$(echo "$1" | tr '[a-z]' '[A-Z]')
