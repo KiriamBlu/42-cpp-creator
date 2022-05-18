6187dcb1ef26cfa1e78a6566f96e530d568f1496
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-19 00:48:47 +0200

    Update template.cpp

--- cpptempcreator/templates/template.cpp
+++ cpptempcreator/templates/template.cpp
@@ -2,16 +2,24 @@
 
 template::template( void ) {
 
+  std::cout << "Default constructor called" << std::endl;
+  
 }
 
 template::template( std::string str ) {
 
+  std::cout << "Parameter constructor called" << std::endl;
+  
 }
 
 template::template( const template & var ) {
 
+  std::cout << "Copy constructor called" << std::endl;
+  
 }
 
 template::template~( void ) {
+  
+  std::cout << "Destructor constructor called" << std::endl;
 
 }
