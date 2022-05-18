78389d6537d6403ed5294b9dec0f2ae9b35d0897
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-18 23:51:53 +0200

    First upload

---
+++ templates/template.cpp
@@ -0,0 +1,17 @@
+#include "template.hpp"
+
+template::template( void ) {
+
+}
+
+template::template( std::string str ) {
+
+}
+
+template::template( const template & var ) {
+
+}
+
+template::template~( void ) {
+
+}
