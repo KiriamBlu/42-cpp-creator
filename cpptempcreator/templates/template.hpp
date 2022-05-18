fb89aaa2a2ff5fe5d67aad681ea7b0cdf5faca51
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-18 23:53:55 +0200

    need update

---
+++ cpptempcreator/templates/template.hpp
@@ -0,0 +1,18 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+#include <string>
+#include <iostream>
+
+class	template {
+
+	private:
+
+	public:
+
+		template ( void );
+		template ( std::string str );
+		template ( const template & var );
+		template~ ( void );
+};
+
+#endif
