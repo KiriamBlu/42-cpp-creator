f3d9a9cf79a7fe939f4f2362fc6ef74619cb6155
Kiriam <kiriamblu@gmail.com>
2022-05-19 04:06:19 +0200

    before try

---
+++ insall.sh
@@ -0,0 +1,15 @@
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+	mkdir /Users/$USER/.cpptemplatecreator
+	git -C /Users/$USER/.cpptemplatecreator clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+elif [[ "$OS" == "Linux" ]]; then
+	git -C /home/$USER/.cpptemplatecreator clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+fi
+
+#echo "alias='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .bashrc 
+echo "alias='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .zshrc
+
+#rm -rf $PWD
+
+cd $OLDPWD 
