1642addb6bfc8b1f617e73f708eeb73a333bdd7d
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-19 20:24:49 +0200

    Update cppcreator.sh

--- cpptempcreator/cppcreator.sh
+++ cpptempcreator/cppcreator.sh
@@ -12,7 +12,7 @@ if [[ "$OS" == "Darwin" ]]; then
 	sed -i '' "s/TEMPLATE/$val/g" $1.hpp
 elif [[ "$OS" == "Linux" ]]; then
 	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
-	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/template.cpp $1.hpp
+	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
 	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/Makefile .
 	sed -i "s/template/$1/g" $1.cpp
 	sed -i "s/template/$1/g" $1.hpp
