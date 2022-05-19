28ed29b35a98451dae23943cd17171b1c4227c0a
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-19 04:37:29 +0200

    Update install.sh

--- cpptempcreator/install.sh
+++ cpptempcreator/install.sh
@@ -1,17 +1,16 @@
+
 OS=$(uname)
 
 if [[ "$OS" == "Darwin" ]]; then
-	mkdir /Users/$USER/.cpptemplatecreator
-	git -C /Users/$USER/.cpptemplatecreator clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+        git -C /Users/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+        #echo "alias cppc='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .bashrc
+        echo "alias cppc='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .zshrc
 elif [[ "$OS" == "Linux" ]]; then
-	git -C /home/$USER/.cpptemplatecreator clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+        git -C /home/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git .cpptemplatecreator
+        #echo "alias cppc='/home/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .bashrc
+        echo "alias cppc='/home/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .zshrc
 fi
 
-#echo "alias cppc='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .bashrc 
-echo "alias cppc='/Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator'" >> .zshrc
+rm -rf $PWD                                                                                  
 
-cd ../
-
-rm -rf $PWD
-
-cd ../
+cd ~/Desktop
