90fc2833d6b90734d32b58066a055dcf8b77d378
Kiriam <kiriamblu@gmail.com>
2022-05-19 04:33:56 +0200

    final I think

--- cpptempcreator/cppcreator.sh
+++ cpptempcreator/cppcreator.sh
@@ -1,7 +1,19 @@
-cp /Users/$USER/cpptempcreator/templates/template.cpp $1.cpp
-cp /Users/$USER/cpptempcreator/templates/template.hpp $1.hpp
-cp /Users/$USER/cpptempcreator/templates/Makefile .
-sed -i '' "s/template/$1/g" $1.cpp
-sed -i '' "s/template/$1/g" $1.hpp
-val=$(echo "$1" | tr '[a-z]' '[A-Z]')
-sed -i '' "s/TEMPLATE/$val/g" $1.hpp
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+	cp /Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
+	cp /Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
+	cp /Users/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/Makefile .
+	sed -i '' "s/template/$1/g" $1.cpp
+	sed -i '' "s/template/$1/g" $1.hpp
+	val=$(echo "$1" | tr '[a-z]' '[A-Z]')
+	sed -i '' "s/TEMPLATE/$val/g" $1.hpp
+elif [[ "$OS" == "Linux" ]]; then
+	cp /home/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
+	cp /home/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/template.cpp $1.hpp
+	cp /home/$USER/.cpptemplatecreator42-cpp-creator/cpptempcreator/templates/Makefile .
+	sed -i "s/template/$1/g" $1.cpp
+	sed -i "s/template/$1/g" $1.hpp
+	val=$(echo "$1" | tr '[a-z]' '[A-Z]')
+	sed -i "s/TEMPLATE/$val/g" $1.hpp
+fi
