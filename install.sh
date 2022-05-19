7f243018143b6570b0d1d4c97d19f68cf974dc37
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-05-19 13:24:51 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+        git -C /Users/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git &
+        #echo "alias cppc='/Users/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator'" >> /.bashrc &
+        echo "alias cppc='/Users/$USER/42-cpp-creator/cpptempcreator'" >> ~/.zshrc &
+elif [[ "$OS" == "Linux" ]]; then
+        git -C /home/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git &
+        #echo "alias cppc='/home/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator'" >> ~/.bashrc &
+        echo "alias cppc='/home/$USER/42-cpp-creator/cpptempcreator'" >> ~/.zshrc &
+fi     
