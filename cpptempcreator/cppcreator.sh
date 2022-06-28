f19fa8c7fef83f5ab2aaf38f27bb08e122463e8d
Joan Sanfelipe <88371986+KiriamBlu@users.noreply.github.com>
2022-06-28 14:40:20 +0200

    Update cppcreator.sh

--- cpptempcreator/cppcreator.sh
+++ cpptempcreator/cppcreator.sh
@@ -1,9 +1,9 @@
 OS=$(uname)
 
 if [[ "$OS" == "Darwin" ]]; then
-	cp /Users/$USER/42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
-	cp /Users/$USER/42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
-	cp /Users/$USER/42-cpp-creator/cpptempcreator/templates/Makefile .
+	cp /Users/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
+	cp /Users/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
+	cp /Users/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/Makefile .
 	sed -i '' "s/template/$1/g" $1.cpp
 	sed -i '' "s/template/$1/g" $1.hpp
 	sed -i '' "s/00000000/$USER/g" $1.cpp
@@ -11,9 +11,9 @@ if [[ "$OS" == "Darwin" ]]; then
 	val=$(echo "$1" | tr '[a-z]' '[A-Z]')
 	sed -i '' "s/TEMPLATE/$val/g" $1.hpp
 elif [[ "$OS" == "Linux" ]]; then
-	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
-	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
-	cp /home/$USER/42-cpp-creator/cpptempcreator/templates/Makefile .
+	cp /home/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/template.cpp $1.cpp
+	cp /home/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/template.hpp $1.hpp
+	cp /home/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/templates/Makefile .
 	sed -i "s/template/$1/g" $1.cpp
 	sed -i "s/template/$1/g" $1.hpp
 	sed -i "s/00000000/$USER/g" $1.cpp
