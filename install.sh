6a9dab85d33229775b4482041dc169282f7886d4
Víctor Martínez Estrada <victormartinezestrada@gmail.com>
2022-07-16 19:09:22 +0200

    Fixed: Installer works. Improvements: Reduced core redundancy

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-
 OS=$(uname)
 
-if [[ "$OS" == "Darwin" ]]; then
-        git -C /Users/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git
-        echo "alias cppc='bash /Users/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/cppcreator.sh'" >> /.bashrc
-        #echo "alias cppc='bash /Users/$USER/42-cpp-creator/cpptempcreator/cppcreator.sh'" >> ~/.zshrc
-elif [[ "$OS" == "Linux" ]]; then
-        git -C /home/$USER/ clone git@github.com:KiriamBlu/42-cpp-creator.git
-        echo "alias cppc='bash /home/$USER/.cpptemplatecreator/42-cpp-creator/cpptempcreator/cppcreator.sh'" >> ~/.bashrc
-        #echo "alias cppc='bash /home/$USER/42-cpp-creator/cpptempcreator/cppcreator.sh'" >> ~/.zshrc
-fi     
+if [[ "$HOME" == "" ]]; then
+        if [[ "$OS" == "Darwin" ]]; then
+                HOME=/Users/$USER
+        elif [[ "$OS" == "Linux" ]]; then
+                HOME=/home/$USER
+        fi
+fi
+
+cp cpptempcreator/ $HOME/.cpptemplatecreator
+echo "alias cppc='bash $HOME/.cpptemplatecreator/cppcreator.sh'" >> ~/.bashrc
+#echo "alias cppc='bash /home/$USER/42-cpp-creator/cpptempcreator/cppcreator.sh'" >> ~/.zshrc
